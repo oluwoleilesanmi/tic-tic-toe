8f791dd4f5787fd23e725a2e53d074c55f5f9ba9
Oluwole Ilesanmi <oluwoleilesanmi@gmail.com>
2019-04-30 01:51:22 +0100

    Included constants for model

--- assests/js/main.js
+++ assests/js/main.js
@@ -13,3 +13,12 @@ let Model = (() => {
     }
     
 })();
+
+const Const = {
+    winPositions: [[1, 2, 3], [4, 5, 6], [7, 8, 9],
+                   [1, 4, 7], [2, 5, 8], [3, 6, 9],
+                   [1, 5, 9], [3, 5, 7]],
+    playerX: 'X',
+    playerO: 'O',
+    mapLength: 9
+}
